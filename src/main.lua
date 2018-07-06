4f09aa5d0a50f629783fb3b4e6e6100ccd62fc78
最萌小汐 <sumneko@hotmail.com>
2018-07-06 10:49:01 +0800

    更新JassParser

--- src/main.lua
+++ src/main.lua
@@ -6,18 +6,17 @@ end)()
 
 require 'filesystem'
 require 'utility'
-local uni  = require 'unicode'
 local parser = require 'parser'
 local optimizer = require 'optimizer'
 
 local function main()
     if arg[1] then
         local exepath  = package.cpath:sub(1, package.cpath:find(';')-6)
-        local root     = fs.path(uni.a2u(exepath)):parent_path():parent_path()
+        local root     = fs.path(exepath):parent_path():parent_path()
         local common   = io.load(root / 'src' / 'jass' / 'common.j')
         local blizzard = io.load(root / 'src' / 'jass' / 'blizzard.j')
 
-        local path = fs.path(uni.a2u(arg[1]))
+        local path = fs.path(arg[1])
         local jass = io.load(path)
 
         local ast, grms
@@ -25,7 +24,7 @@ local function main()
             ast, grms = parser(common,   'common.j',   ast)
             ast, grms = parser(blizzard, 'blizzard.j', ast)
             ast, grms = parser(jass,     'war3map.j',  ast)
-        end, error_handle)
+        end, debug.traceback)
 
         config = {}
         config.confusion = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_'
